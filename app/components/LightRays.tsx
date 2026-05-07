cc31b0244af48a3c96e7bdfe01ddaa6049c353f5
doaa mohamed <doaatawfik82001@gmail.com>
2026-05-06 17:51:09 -0700

    Initial commit: setup Next.js app with app router and global styles

---
+++ app/components/LightRays.tsx
@@ -0,0 +1,17 @@
+import LightRays from "../../components/LightRays";
+
+<LightRays
+  raysOrigin="top-center"
+  raysColor="#5dfeca"
+  raysSpeed={1}
+  lightSpread={0.5}
+  rayLength={3}
+  followMouse={true}
+  mouseInfluence={0.1}
+  noiseAmount={0}
+  distortion={0}
+  className="custom-rays"
+  pulsating={false}
+  fadeDistance={1}
+  saturation={1}
+/>;
